efb368ea5d8022047c5a2c0da9d49d3d362bbd64
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-11-27 10:46:28 -0300

    autenticação

--- src/main/resources/static/js/firebaseConfig.js
+++ src/main/resources/static/js/firebaseConfig.js
@@ -1,15 +1,8 @@
-/**
- * Configuração: configuração do Projeto Firebase.
- * Altere essas configurações conforme seu projeto no Firebase.com
- *  - Entre no console do projeto no Firebase
- *  - Clique na engrenagem
- *  - Copie os dados do projeto nas chaves abaixo
- */
 const firebaseConfig = {
-  apiKey: "AIzaSyD2KkOlBLAIhMM8JRjLI3V0t9A07WmyAYo",
-  authDomain: "padcoisas-32486.firebaseapp.com",
-  projectId: "padcoisas-32486",
-  storageBucket: "padcoisas-32486.firebasestorage.app",
-  messagingSenderId: "659362551657",
-  appId: "1:659362551657:web:8bf9f2ef66ef5b858d4ff8"
+  apiKey: "AIzaSyD59UHSLvL7XKtAaXXZRBaLB2wGrOmBykc",
+  authDomain: "springpad-77484.firebaseapp.com",
+  projectId: "springpad-77484",
+  storageBucket: "springpad-77484.firebasestorage.app",
+  messagingSenderId: "259672331663",
+  appId: "1:259672331663:web:202f63b9284b71a39fa7fa"
 };
